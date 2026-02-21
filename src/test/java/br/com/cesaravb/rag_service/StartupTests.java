1c3a6a4dffd682d9c5230ede1506d71abb3ee989
César Augusto <cesar.augusto.rj1@gmail.com>
2026-02-21 06:52:05 -0300

    chore: inicializa a estrutura do projeto com configuração do Maven e Spring Boot
    - Adiciona o Maven Wrapper e propriedades para gerenciamento do projeto
    -Cria Dockerfile com build multi-stage
    -Configura workflow do GitHub Actions para releases automatizadas
    -Adiciona configuração inicial da aplicação e arquivos de propriedades
    -Implementa aplicação básica Spring Boot com classe de inicialização
    -Inclui dependências necessárias para Spring Data JPA, Web e integração
    com IA
    - Define .gitignore e .gitattributes para consistência do ambiente

---
+++ src/test/java/br/com/cesaravb/rag_service/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.cesaravb.rag_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
